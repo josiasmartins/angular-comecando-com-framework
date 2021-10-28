40361f61c03b3fe9913adcc619593047eb4ae579
Caitano <josiasmartinscaitano@gmail.com>
2021-10-28 07:21:01 -0300

    utilizado o input e o ngFor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
-import { NgModule } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
 import { ExtratoComponent } from './extrato/extrato.component';
+import { registerLocaleData } from '@angular/common';
+import localetPt from '@angular/common/locales/pt';
 
+// ela precisa de 3 parâmetro: o local que preciso importar e qual a chave
+registerLocaleData(localetPt, 'pt');
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +20,15 @@ import { ExtratoComponent } from './extrato/extrato.component';
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [
+    // tambem preciso dizer de onde vem isso
+    {provide: LOCALE_ID, useValue: 'pt'},
+    // injetou o formatador de moedas do pipe
+    {
+      provide: DEFAULT_CURRENCY_CODE,
+      useValue: 'BRL'
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
